7beb022615f90e60791d3423d82d1e28de294c4a
Luke Moscrop <luke.moscrop@bbc.co.uk>
2016-01-10 16:43:04 +0000

    Added jade template compiler for HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
     normalize = require('node-normalize-scss').includePaths,
     neat = require('node-neat').includePaths,
     bourbon = require('node-bourbon').includePaths;
@@ -12,9 +13,18 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['assets/sass/*.scss', 'assets/sass/**/*.scss'], ['styles']);
+gulp.task('templates', function() {
+    return gulp.src('assets/jade/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public'));
 });
 
-gulp.task('default', ['styles', 'watch']);
-gulp.task('deploy', ['styles']);
+gulp.task('watch', function() {
+    gulp.watch(['assets/sass/*.scss', 'assets/sass/**/*.scss'], ['styles']);
+    gulp.watch(['assets/jade/*.jade', 'assets/jade/**/*.jade'], ['templates']);
+});
+
+gulp.task('default', ['styles', 'templates', 'watch']);
+gulp.task('deploy', ['styles', 'templates']);
